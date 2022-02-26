20fa37908c6893641466d6400fe52aa386d9583e
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-02-26 14:20:04 -0800

    fixed typo

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -14,17 +14,17 @@ export class WeatherService {
 
   getCurrentWeather(city: string, country: string) {
       return this.httpClient.get<ICurrentWeatherData>
-      (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`).pipe(map(data => this.transformToIcurrentWeather(data)))
+      (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`)
+      .pipe(map(data => this.transformToIcurrentWeather(data) ))
       
 
   }
 
-private transformToIcurrentWeather(data:
-  ICurrentWeatherData) {
+private transformToIcurrentWeather(data:ICurrentWeatherData) {
     return {
       city: data.name,
       country: data.sys.country,
-      Date: new Date(data.dt * 1000),
+      date: new Date(data.dt * 1000),
       temperature: data.main.temp * 9/5 - 459.67,
       description: data.weather[0].description,
       Image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
