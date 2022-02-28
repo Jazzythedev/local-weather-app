9d0c49965e6231f48adbcf7eaf00aa2c92e22751
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-02-28 08:49:24 -0800

    event added

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ICurrentWeather } from '../icurrent-weather';
-import { WeatherService } from '../weather.service';
+
 
 @Component({
   selector: 'app-current-weather',
@@ -9,8 +9,8 @@ import { WeatherService } from '../weather.service';
 })
 export class CurrentWeatherComponent implements OnInit {
   
-  current: ICurrentWeather
-  constructor(private WeatherService: WeatherService) { 
+  @Input() current: ICurrentWeather
+  constructor() { 
     this.current = {
       city: '',
       country: '',
@@ -23,7 +23,5 @@ export class CurrentWeatherComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void { 
-    this.WeatherService.getCurrentWeather('Redmond', 'US').subscribe(data => this.current = data)
-  }
+  ngOnInit(): void {}
 } 
