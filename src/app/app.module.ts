d16384f2d10377821f37787995a36e3fc5578082
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-02-27 19:54:28 -0800

    reintroduced imports and declared citysearchcomp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { CitysearchComponent } from './citysearch/citysearch.component';
+import { MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule} from '@angular/material/form-field'; 
+import { MatInputModule} from '@angular/material/input';
+
+
+
+
+
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    CitysearchComponent
   ],
   imports: [
     BrowserModule,
